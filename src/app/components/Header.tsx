cf3c07f24f32f0afa2f9b5eda7f0f893441f02ea
awkwardusername <awkwardusername@gmail.com>
2025-08-10 15:54:15 +0800

    Revert "obfuscate contacts"
    This reverts commit d951f51bf36cdefb2cae82caa5883567b163e7af.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
-import Obfuscate from 'react-obfuscate';
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -74,19 +73,19 @@ export default function Header() {
             </Link>
           </div>
           <div className="pt-4 mt-4 border-t border-gray-700">
-            <p className="flex items-center py-2 hover:text-blue-400 transition">
+            <a href="mailto:info@justbecause.ph" className="flex items-center py-2 hover:text-blue-400 transition">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
               </svg>
-              <Obfuscate email="info@justbecause.ph" />
-            </p>
-            <p className="flex items-center py-2 hover:text-blue-400 transition">
+              info@justbecause.ph
+            </a>
+            <a href="tel:+639524807466" className="flex items-center py-2 hover:text-blue-400 transition">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
-              <Obfuscate tel="+63 952 480 7466" />
-            </p>
+              +63 952 480 7466
+            </a>
           </div>
         </div>
       )}
