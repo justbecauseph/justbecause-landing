7d6ab9c39e110712430913bae4ae2f9452e613e9
awkwardusername <awkwardusername@gmail.com>
2025-08-10 13:11:31 +0800

    Integrate Cloudflare workers

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -21,11 +21,11 @@ export default function Header() {
             </svg>
             info@justbecause.ph
           </a>
-          <a href="tel:+639171234567" className="hidden md:flex items-center hover:text-blue-400 transition">
+          <a href="tel:+639524807466" className="hidden md:flex items-center hover:text-blue-400 transition">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
               <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
             </svg>
-            +63 917 123 4567
+            +63 952 480 7466
           </a>
         </div>
         
@@ -80,11 +80,11 @@ export default function Header() {
               </svg>
               info@justbecause.ph
             </a>
-            <a href="tel:+639171234567" className="flex items-center py-2 hover:text-blue-400 transition">
+            <a href="tel:+639524807466" className="flex items-center py-2 hover:text-blue-400 transition">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
-              +63 917 123 4567
+              +63 952 480 7466
             </a>
           </div>
         </div>
