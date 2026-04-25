17bd7a071fc75fcbc59807b1255084c2a214cba9
Mark Jayson Fuentes <awkwardusername@gmail.com>
2026-04-25 10:51:57 +0800

    fix: correct capture_pageview format in PostHog initialization

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,6 +2,6 @@ import posthog from "posthog-js"
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  capture_pageview: "history-change",
+  capture_pageview: 'history_change',
   capture_pageleave: true,
 })
